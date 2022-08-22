ec3af7f1cf281ead89879c3c1d12df66cc76c493
samuelnihoul <contact@harmonia-eko.oooo>
2022-08-22 18:24:21 +0000

    pau

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 //  Query https://datausa.io/api/data?drilldowns=State&measures=Population&year=2019 using React Query
 // it has to be displayed in 5 columns of cards
 import { useQuery } from 'react-query';
+
 export default function App() {
 
   const { isLoading, error, data } = useQuery('repoData', () =>
@@ -8,22 +9,23 @@ export default function App() {
     )
   )
 
-  return (isLoading ? <p>Loading...</p> : error ? <p>Error</p> :
-    <><h1 className=''>Population of the States</h1>
+  return (isLoading ? <p>Loading...</p> : error ? <p>Error</p> : <div className='bg-gray-300 min-h-screen' >
+    <h1 className='h-16 not-italic font-bold text-5xl text-teal-500 ml-60 mt-40'>Population of the States</h1>
 
-      <div className="bg-basic-blue flex justify-center items-center min-h-screen grid grid-cols-5">
-
-
-
-        {data.data.slice(0, 12).map(({ State, Year, Population }: { State: string, Year: number, Population: number }) => {
-          console.log(Year); return <div className='block p-6 rounded-lg shadow-lg bg-white max-w-sm flex columns-1'>
-
-            <span>{State}</span><hr></hr><span>{Year.toString()}</span><hr></hr><span>{Population.toString()}</span>
-          </div>
-        })}
+    <div className=" grid grid-cols-5">
 
 
-      </div></>
+
+      {data.data.slice(0, 12).map(({ State, Year, Population }: { State: string, Year: number, Population: number }) => {
+        console.log(Year); return <div className='block p-6 rounded-lg shadow-lg bg-white max-w-sm flex  m-6'>
+          <div className='columns-1'>
+            <div className='font-bold'>{State}</div><hr></hr>
+            <div className='font-bold text-gray-600'>Population: {Math.round(Population / 100000) / 10}M</div><hr className='w-48 justify-center'></hr><div className='font-bold text-gray-600'>Year: {Year}</div>
+          </div></div>
+      })}
+
+
+    </div></div>
   );
 }
 
